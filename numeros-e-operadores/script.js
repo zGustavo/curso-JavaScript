1f669067108ecc6aa064d3663cc7a5fce36fdcb7
Gustavo Dias <gustavo.diias1@gmail.com>
2021-06-04 21:09:58 -0300

    Aula - 3 Números e Operadores

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,32 @@
+var total = 10 + 5 + 10;
+var divisao = 100 / 5;
+var modulo = 3872982354545914 % 3;
+
+var teste = '10' - 5; // Converte e subtrai
+
+console.log(modulo);
+console.log(teste);
+
+/* 
+Exercício
+Qual o resultado da seguinte expressão? */
+var total = 10 + 5 * 2 / 2 + 20; //35
+
+// Crie duas expressões que retornem NaN
+let expressao1 = 'Isso é um NaN' / 2 ;
+let expressao2 = "Isso também é um NaN" * 2;
+
+// Somar a String '200' com número 50 e retornar 250
+let soma = ('200' * 1) + 50;
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var numero = 5;
+console.log(++numero);
+
+// Como dividir o peso por 2?
+var numero = '80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso; // NaN (Not a Number)
+
+console.log(peso);
