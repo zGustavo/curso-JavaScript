04ecec15aa7cd6a622298dc0587e2a9bbadb451e
Gustavo Dias <gustavo.diias1@gmail.com>
2021-06-05 23:32:13 -0300

    Aula 4 - Boolean a Condicionais

--- boolean-e-condicionais/script.js
+++ boolean-e-condicionais/script.js
@@ -9,11 +9,35 @@ if (fulano > beltrano) {
 // Dependendo do resultado coloque no console 'É maior', 'É igual
 // Qual valor é retornado na seguinte expressão?
 var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
- console.log(expressao)
+// 3 Pois as 3 condições são TRUE logo ele mostra a ultima condição 3
+ 
 // Verifique se as seguintes variáveis são Truthy ou Falsy
-var nome = 'Andre';
-var idade = 28;
-var possuiDoutorado = false;
-var empregoFuturo;
-var dinheiroNaConta = 0;
+var nome = 'Andre'; // Truthy
+var idade = 28; // Truthy
+var possuiDoutorado = false; // Falsy
+var empregoFuturo; // Falsy
+var dinheiroNaConta = 0; // Falsy
 // Compare o total de habitantes do Brasil com China (valor em mi
+
+var brasil = 207;
+var china = 1340;
+
+if(brasil > china) {
+    console.log("Brasil tem mais habitantes");
+} else {
+    console.log("Falso");
+}
+
+// O que irá aparecer no console
+if(('Gato' === 'gato') && (5 > 2)) {
+    console.log("verdadeiro");
+} else {
+    console.log("Falso");
+}
+
+// O que irá aparecer no console
+if(('Gato' === 'gato') || (5 > 2)){
+    console.log('Gato' && 'Cão');
+} else {
+    console.log('Falso');
+}
