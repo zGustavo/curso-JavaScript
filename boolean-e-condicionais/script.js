f5af837b6ec35d65583223c5d0e3a1bf06742a0a
Gustavo Dias <gustavo.diias1@gmail.com>
2021-06-05 23:08:01 -0300

    Aula - 4 Boolean e Condicionais

---
+++ boolean-e-condicionais/script.js
@@ -0,0 +1,19 @@
+// Verifique se a sua idade é maior do que a de algum parente
+var fulano = 28; 
+var beltrano = 30; 
+if (fulano > beltrano) {
+    console.log("Fulano tem idade maior")
+} else {
+    console.log("beltrano é mais velho");
+}
+// Dependendo do resultado coloque no console 'É maior', 'É igual
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+ console.log(expressao)
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+// Compare o total de habitantes do Brasil com China (valor em mi
