025f25b69eec0f903eab9f52dffa26902b9e4d08
Gustavo Dias <gustavo.diias1@gmail.com>
2021-06-14 14:36:23 -0300

    Aula 8 - Arrays e Loops

--- Aula - 7 Tudo-e-Objetos/script.js
+++ Aula - 7 Tudo-e-Objetos/script.js
@@ -12,3 +12,22 @@ btn.innerText; // Pega o valor "Clique aqui!"
 btn.addEventListener('click', function() {
     console.log('Clicou!');
 }); // Quando clicar no link, apareceu no console a mensagem
+
+
+// Exercícios
+// nomeie 3 propriedades ou métodos de strings
+/* Resposta
+    fixed
+    length
+    slice 
+*/
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+/*  addEventListener
+    appendChild
+    id
+    innerHTML
+    outerHTML
+*/
+// busque na web um objeto (método) capaz de interagir com o clip
+// clipboard é a parte do seu computador que lida com o CTRL + C
