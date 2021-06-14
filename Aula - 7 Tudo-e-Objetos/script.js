8ee8ef6b11faa41fdd261f0703a6f43368c65aa0
Gustavo Dias <gustavo.diias1@gmail.com>
2021-06-14 14:34:32 -0300

    Aula 7 Tudo é Objeto

--- Aula - 7 Tudo-e-Objetos/script.js
+++ Aula - 7 Tudo-e-Objetos/script.js
@@ -4,3 +4,11 @@ var numero = 1.8;
 
 numero.toString() // 1.8 mostra como string
 numero.toFixed(); // 2
+
+var btn = document.querySelector('.btn');
+
+btn.classList.add('azul'); // Adiciona a classe azul
+btn.innerText; // Pega o valor "Clique aqui!"
+btn.addEventListener('click', function() {
+    console.log('Clicou!');
+}); // Quando clicar no link, apareceu no console a mensagem
