808d5eb4d10a0f92eabe7779a6168bd53ebe1fc4
Gustavo Dias <gustavo.diias1@gmail.com>
2021-06-15 12:41:59 -0300

    Aula - 11 O que é DOM

---
+++ DOM para Iniciantes/Aula - 11 O-que-e-DOM/script.js
@@ -0,0 +1,18 @@
+const href = window.location.href;
+
+console.log(href);
+
+//Exercício
+// Retorne o url da página atual utilizando o objeto window
+const paginaAtual = window.location.href;
+console.log(paginaAtual);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo');
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+// Retorne a largura da janela 
+const larguraTela = window.innerWidth;
+console.log(larguraTela);
