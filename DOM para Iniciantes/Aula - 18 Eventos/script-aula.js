cf9fa63dc817ab01ae7025f52b7bfd34d0de9c16
Gustavo Dias <gustavo.diias1@gmail.com>
2021-06-21 22:54:10 -0300

    Aula 18 - Eventos

---
+++ DOM para Iniciantes/Aula - 18 Eventos/script-aula.js
@@ -0,0 +1,49 @@
+//  Callback
+const img = document.querySelector('img');
+
+function callback(event) {
+    console.log(event);
+}
+
+img.addEventListener('click', callback)
+
+
+
+const h1 = document.querySelector('h1');
+
+function handleEvent(event) {
+    console.log(event.type, event)
+}
+
+// h1.addEventListener('click', handleEvent);
+// h1.addEventListener('mouseenter', handleEvent);
+// h1.addEventListener('mousemove', handleEvent);
+
+// window.addEventListener('scroll', handleEvent);
+// window.addEventListener('resize', handleEvent);
+// window.addEventListener('keydown', handleEvent);
+
+function handleKeyBoard(event) {
+    if(event.key === 'a') {
+        document.body.classList.toggle('azul');
+    }
+    // console.log(event.key);
+}
+
+window.addEventListener('keydown', handleKeyBoard);
+
+
+const imgs = document.querySelectorAll('img');
+
+
+function handleImg(event) {
+    console.log(event.currentTarget.getAttribute('src'));
+}
+
+imgs.forEach((item) => {
+    item.addEventListener('click', handleImg);
+});
+
+
+
+
--- DOM para Iniciantes/Aula - 18 Eventos/script.js
+++ DOM para Iniciantes/Aula - 18 Eventos/script.js
@@ -1,37 +1,50 @@
-//  Callback
-const img = document.querySelector('img');
+//Exercício
 
-function callback(event) {
-    console.log(event);
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+function addAtivo(event) {
+    event.preventDefault(); // Previne comportamento padrão do link
+    linksInternos.forEach((item) => {
+        item.classList.remove('ativo');
+    })
+    event.currentTarget.classList.add('ativo');
 }
 
-img.addEventListener('click', callback)
+linksInternos.forEach((item) => {
+    item.addEventListener('click', addAtivo);
+})
 
 
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+const todosElementos = document.querySelectorAll('body *'); // pega todos os elementos a partir do body
 
-const h1 = document.querySelector('h1');
+// function handleElemento(event) {
+//     console.log(event.currentTarget);
+// }
 
-function handleEvent(event) {
-    console.log(event.type, event)
-}
+// todosElementos.forEach((item) => {
+//     item.addEventListener('click', handleElemento)
+// })
 
-// h1.addEventListener('click', handleEvent);
-// h1.addEventListener('mouseenter', handleEvent);
-// h1.addEventListener('mousemove', handleEvent);
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
 
-// window.addEventListener('scroll', handleEvent);
-// window.addEventListener('resize', handleEvent);
-// window.addEventListener('keydown', handleEvent);
+// function handleElemento(event) {
+//     event.currentTarget.remove(); // Alterando a função anterior para remover
+// }
 
-function handleKeyBoard(event) {
-    if(event.key === 'a') {
-        document.body.classList.toggle('azul');
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
+function handleClickT(event) {
+    console.log(event.key);
+    if(event.key === 't') {
+        document.documentElement.classList.toggle('textomaior');
     }
-    // console.log(event.key);
 }
 
-window.addEventListener('keydown', handleKeyBoard);
-
-
-
-
+window.addEventListener('keydown', handleClickT);
