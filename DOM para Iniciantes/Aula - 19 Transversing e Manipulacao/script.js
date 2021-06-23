8bafbe6b881b491a77e80ff50447835bf7630e54
Gustavo Dias <gustavo.diias1@gmail.com>
2021-06-22 23:06:27 -0300

    Aula 19 Transversing e Manipulação

---
+++ DOM para Iniciantes/Aula - 19 Transversing e Manipulacao/script.js
@@ -0,0 +1,9 @@
+const mapa = document.querySelector('.mapa');
+
+// Criar Elementos
+
+const novoh1 = document.createElement('h1');
+novoh1.innerText = 'Novo título';
+novoh1.classList.add('titulo');
+
+mapa.appendChild(novoh1);
